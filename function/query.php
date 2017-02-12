caeb9eff8ad4f4f9c15155c69feff308f135dbca
Alfred <y_kcheok@hotmail.com>
2017-02-12 17:18:32 +0800

    open debug log in query

--- function/query.php
+++ function/query.php
@@ -1,11 +1,13 @@
 <?php
 
+include "system/function.php";
+
 function testQuery() {
     global $dbhandler0, $db, $table;
     mysql_query("SET NAMES 'utf8'");
     $sqlcheck = "SELECT * FROM " . $db["mysql"] . "." . $table["columns_priv"];
     $res = $dbhandler0->query($sqlcheck);
-    //WriteDebugLog($sqlcheck, DEBUG_QUERY);
+    WriteDebugLog($sqlcheck, DEBUG_QUERY);
     return $res;
 }
 
